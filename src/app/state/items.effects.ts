e85cc6d46414d5886242778172afd078fcec1cdf
Osama Yousry <osama.yousry@outlook.com>
2021-01-23 23:15:13 +0200

    Remove unused dependencies

--- src/app/state/items.effects.ts
+++ src/app/state/items.effects.ts
@@ -2,10 +2,7 @@ import { Injectable } from "@angular/core";
 import { Actions, createEffect, ofType } from '@ngrx/effects';
 import { ItemsServiceService } from "../services/items-service.service";
 import { EMPTY } from 'rxjs';
-import { map, mergeMap, catchError, exhaustMap, withLatestFrom, tap } from 'rxjs/operators';
-import { Store } from "@ngrx/store";
-import { AppState } from "./app.state";
-import { selectItems } from "./items.selectors";
+import { map, exhaustMap } from 'rxjs/operators';
 import { retrievedItems, updateItems } from "./items.actions";
 
 
