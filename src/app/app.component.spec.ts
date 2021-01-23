2100aa170b99aa586b5a891ddb578baba586ade6
Osama Yousry <osama.yousry@outlook.com>
2021-01-23 15:25:53 +0200

    Remove unnecessary test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -21,11 +21,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app.title).toEqual('gilded-rose-fe');
   });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('gilded-rose-fe app is running!');
-  });
 });
