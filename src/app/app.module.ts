ae346dfa8b4aa7db881cb7dd2a30b282f5f6ae7b
Osama Yousry <osama.yousry@outlook.com>
2021-01-23 15:18:34 +0200

    Add slim scroll

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,9 @@ import { MatInputModule } from '@angular/material/input';
 import { TableComponent } from './components/table/table.component';
 import { InputFormComponent } from './components/inputForm/input-form/input-form.component';
 import { MatButtonModule } from '@angular/material/button';
+import { NgSlimScrollModule } from 'ngx-slimscroll';
+import { SLIMSCROLL_DEFAULTS } from 'ngx-slimscroll';
+
 
 
 
@@ -24,9 +27,16 @@ import { MatButtonModule } from '@angular/material/button';
     MatCardModule,
     MatTableModule,
     MatInputModule,
-    MatButtonModule
+    MatButtonModule,
+    NgSlimScrollModule
   ],
-  providers: [],
+  providers: [{
+    provide: SLIMSCROLL_DEFAULTS,
+    useValue: {
+      barBackground: '#adb5bd', gridOpacity: 0.1, barWidth: 5,
+      gridWidth: 1
+    }
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
