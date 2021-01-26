da3660627a1883f86a016cea5c74cf9d418d34e2
Osama Yousry <osama.yousry@outlook.com>
2021-01-26 22:30:29 +0200

    Write tests for app component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,13 +1,23 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { provideMockStore, MockStore } from '@ngrx/store/testing';
+import { By } from '@angular/platform-browser';
+import { updateItems } from './state/items.actions';
+import { selectLoading } from './state/items.selectors';
+
 
 describe('AppComponent', () => {
+  let store: MockStore;
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [
         AppComponent
       ],
+      providers: [
+        provideMockStore({ initialState: { items: { loading: false } } }),
+      ],
     }).compileComponents();
+    store = TestBed.inject(MockStore);
   });
 
   it('should create the app', () => {
@@ -16,9 +26,36 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'gilded-rose-fe'`, () => {
+  it(`should have a title 'Gilded Rose Inventory'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('gilded-rose-fe');
+    const title = fixture.debugElement.query(By.css("mat-card-title")).nativeElement;
+    expect(title.innerHTML).toContain('Gilded Rose Inventory');
   });
+
+  it('should have a loading bar in case of loading state', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    store.setState({ items: { loading: true } });
+    store.refreshState();
+    fixture.detectChanges();
+    const loadingBar = fixture.debugElement.query(By.css("mat-progress-bar")).nativeElement;
+    expect(loadingBar).toBeDefined();
+  });
+
+  it('should remove loading bar in case of no loading state', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    store.setState({ items: { loading: false } });
+    store.refreshState();
+    fixture.detectChanges();
+    const loadingBar = fixture.debugElement.query(By.css("mat-progress-bar"));
+    expect(loadingBar).toBeNull();
+  });
+
+  it('should dispatch update action on button click', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const button = fixture.debugElement.query(By.css("button.end-button")).nativeElement;
+    const dispatchSpy = spyOn(store, 'dispatch');
+    const expectedAction = updateItems();
+    button.click();
+    expect(dispatchSpy).toHaveBeenCalledOnceWith(expectedAction);
+  })
 });
